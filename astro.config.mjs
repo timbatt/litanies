7e332622db746bbb0e89c6ae20160200c8cc71e4
Timothy Batt <timothybatt3@gmail.com>
2024-01-12 22:55:58 -0500

    added Holy Spirit litany

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: 'https://zvsoftware.github.io',
+  // base: '/',
+  server: {
+    port: 8000
+  }
+})
