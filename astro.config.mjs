0fa4bb37162d1e0e5c1b7f80f61b5c5ae1650343
Timothy Batt <timothybatt3@gmail.com>
2024-01-12 23:20:50 -0500

    TEST: Changed site root

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://zvsoftware.github.io',
-  base: '/projects/litanies',
+  site: 'https://zvsoftware.github.io/projects',
+  base: '/litanies',
   server: {
     port: 8000
   }
